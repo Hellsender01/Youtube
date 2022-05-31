a6a3cb34261eae421376a933805a8d3b035508af
Harshit Joshi <57838029+Hellsender01@users.noreply.github.com>
2022-05-31 11:08:42 +0530

    Add files via upload

---
+++ Binary Exploitation/7. Ret2LIBC/ret2libc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void init() {
+  gid_t gid;
+  uid_t uid;
+
+  gid = getegid();
+  uid = geteuid();
+
+  setresgid(gid, gid, gid);
+  setresuid(uid, uid, uid);
+}
+
+int main() {
+	init();
+	char buff[30];
+	printf("Enter Data - ");
+	gets(buff);
+	return 0;
+}
